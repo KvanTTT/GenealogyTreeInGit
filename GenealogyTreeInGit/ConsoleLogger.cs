2d85b5c139e9f08b17de529a7244159e8d2d85c5
Ivan Kochurkin <kvanttt@gmail.com>
2018-09-02 15:17:05 +0300

    Add ILogger and ConsoleLogger

---
+++ GenealogyTreeInGit/ConsoleLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenealogyTreeInGit
+{
+    class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+
+        public void LogInfo(string message)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+    }
+}
---
+++ GenealogyTreeInGit/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace GenealogyTreeInGit
+{
+    public interface ILogger
+    {
+        void LogError(string message);
+
+        void LogInfo(string message);
+    }
+}
